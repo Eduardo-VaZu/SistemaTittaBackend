5db251b347b3464423141bf3f09cfd742bcc5afd
YazminScs <ysuybatecalderon@gmail.com>
2025-08-21 15:50:42 -0500

    Configuración inicial del proyecto

---
+++ src/main/java/com/sistemaBackend/SistemaTittaBackend/SistemaTittaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaBackend.SistemaTittaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTittaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTittaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaBackend/SistemaTittaBackend/SistemaTittaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaBackend.SistemaTittaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTittaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
