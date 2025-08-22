8e367ca3ffebb32f3cf9e09533018d3e0fa29e3f
Eduardo-VaZu <u22234544@utp.edu.pe>
2025-08-21 21:26:47 -0500

    Agregar clases 'StockSede' y 'StockSedeId' con sus atributos

--- src/main/java/com/sistemaBackend/SistemaTittaBackend/model/StockSede.java
+++ src/main/java/com/sistemaBackend/SistemaTittaBackend/model/StockSede.java
@@ -1,4 +1,12 @@
 package com.sistemaBackend.SistemaTittaBackend.model;
 
 public class StockSede {
+
+    private StockSedeId id;
+
+    private Producto producto;
+
+    private Sede sede;
+
+    
 }
